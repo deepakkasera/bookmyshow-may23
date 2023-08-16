b845cefde21411221ff1ec885974adbab090924a
deepakkasera <deepak.kasera143.dk@gmail.com>
2023-08-17 00:03:23 +0530

    BMS code

--- src/main/java/com/scaler/bookmyshowmay23/repositories/ShowSeatRepository.java
+++ src/main/java/com/scaler/bookmyshowmay23/repositories/ShowSeatRepository.java
@@ -1,9 +1,11 @@
-package com.scaler.bookmyshowmay23;
+package com.scaler.bookmyshowmay23.repositories;
 
 import com.scaler.bookmyshowmay23.models.Show;
 import com.scaler.bookmyshowmay23.models.ShowSeat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +15,8 @@ public interface ShowSeatRepository extends JpaRepository<ShowSeat, Long> {
     Optional<ShowSeat> findById(Long showSeatId);
     //select * from show_seat where id = showSeatId
 
+    //select * from show_seat where Id in (....) for update.
+    @Lock(LockModeType.PESSIMISTIC_READ)
     List<ShowSeat> findAllByIdIn(List<Long> showSeatId);
     //select * from show_seat where id IN (4,5,6,7);
 
--- src/main/java/com/scaler/bookmyshowmay23/services/TicketService.java
+++ src/main/java/com/scaler/bookmyshowmay23/services/TicketService.java
@@ -1,27 +1,34 @@
 package com.scaler.bookmyshowmay23.services;
 
-import com.scaler.bookmyshowmay23.ShowSeatRepository;
+import com.scaler.bookmyshowmay23.models.*;
+import com.scaler.bookmyshowmay23.repositories.ShowSeatRepository;
 import com.scaler.bookmyshowmay23.exceptions.ShowSeatNotFoundException;
-import com.scaler.bookmyshowmay23.models.ShowSeat;
-import com.scaler.bookmyshowmay23.models.ShowSeatState;
-import com.scaler.bookmyshowmay23.models.Ticket;
+import com.scaler.bookmyshowmay23.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TicketService {
     private ShowSeatRepository showSeatRepository;
+    private UserRepository userRepository;
 
-    TicketService(ShowSeatRepository showSeatRepository) {
+    TicketService(ShowSeatRepository showSeatRepository,
+                  UserRepository userRepository) {
         this.showSeatRepository = showSeatRepository;
+        this.userRepository = userRepository;
     }
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public Ticket bookTicket(List<Long> showSeatIds,
                              Long userId,
                              Long showId) throws ShowSeatNotFoundException {
         //Steps:-
 
-        //1. Fetch show Seats from the list of showSeatIds.
+        //1. Fetch show Seats from the list of showSeatIds. (No lock)
         List<ShowSeat> showSeats = showSeatRepository.findAllByIdIn(showSeatIds);
 
         //2. Check the status of Show Seats.
@@ -33,15 +40,35 @@ public class TicketService {
             }
         }
 
-        //4. Acquire the lock.
+        //4. Acquire the lock. -> call findAllByIdIn() with LOCK.
+        //5. Check the status of Show Seats again.
+        showSeats = showSeatRepository.findAllByIdIn(showSeatIds); //WITH LOCK.
+
+        //Check the status again for the all the show seats.
+
         for (ShowSeat showSeat : showSeats) {
             showSeat.setState(ShowSeatState.LOCKED);
             showSeatRepository.save(showSeat);
         }
-
-        //5. Check the status of Show Seats again.
         //6. If all the seats are AVAILABLE, then generate the Ticket.
 
-        return null;
+        Ticket ticket = new Ticket();
+
+        Optional<User> userOptional = userRepository.findById(userId);
+
+        if (userOptional.isEmpty()) {
+            //
+        }
+
+        ticket.setBookedBy(userOptional.get());
+        ticket.setTicketStatus(TicketStatus.PENDING); //Ticket Status will be CONFIRMED only after payment.
+        ticket.setShowSeats(showSeats);
+
+        //PaymentService.
+
+//        if (paymentStatus != SUCCESS) {
+//            rollback the entire transaction.
+//        }
+        return ticket;
     }
 }
